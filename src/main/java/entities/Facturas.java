7166dcd306daa68486bd9cf841fc608ed9f4cf2d
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-05-11 12:48:50 +0200

    Hago las entidades y los controladores jpa. Empiezo la interfaz

---
+++ src/main/java/entities/Facturas.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author jorge
+ */
+@Entity
+@Table(name = "facturas")
+@NamedQueries({
+    @NamedQuery(name = "Facturas.findAll", query = "SELECT f FROM Facturas f"),
+    @NamedQuery(name = "Facturas.findByIdFactura", query = "SELECT f FROM Facturas f WHERE f.idFactura = :idFactura"),
+    @NamedQuery(name = "Facturas.findByFechaEmision", query = "SELECT f FROM Facturas f WHERE f.fechaEmision = :fechaEmision"),
+    @NamedQuery(name = "Facturas.findByDescripcion", query = "SELECT f FROM Facturas f WHERE f.descripcion = :descripcion"),
+    @NamedQuery(name = "Facturas.findByImporteTotal", query = "SELECT f FROM Facturas f WHERE f.importeTotal = :importeTotal")})
+public class Facturas implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "idFactura")
+    private Integer idFactura;
+    @Column(name = "fechaEmision")
+    @Temporal(TemporalType.DATE)
+    private Date fechaEmision;
+    @Column(name = "descripcion")
+    private String descripcion;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "importeTotal")
+    private Double importeTotal;
+
+    public Facturas() {
+    }
+
+    public Facturas(Integer idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public Integer getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(Integer idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public Date getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(Date fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getImporteTotal() {
+        return importeTotal;
+    }
+
+    public void setImporteTotal(Double importeTotal) {
+        this.importeTotal = importeTotal;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idFactura != null ? idFactura.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Facturas)) {
+            return false;
+        }
+        Facturas other = (Facturas) object;
+        if ((this.idFactura == null && other.idFactura != null) || (this.idFactura != null && !this.idFactura.equals(other.idFactura))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "persistencia.Facturas[ idFactura=" + idFactura + " ]";
+    }
+    
+}
